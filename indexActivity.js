efb79ff1587b912cda617ce1c7963a2f0fb03ae6
ivan castro <castroivanrei@gmail.com>
2023-03-21 14:02:28 +0800

    230320

---
+++ indexActivity.js
@@ -0,0 +1,19 @@
+console.log('Hello, Welcome to De La Salle University');
+let fName = 'Ivan';
+let lName = 'Castro';
+let age = 22;
+let hobbies = ['sleeping', 'running', 'gaming', 'watching', 'listening to music'];
+let homeAddress = {
+	houseNumber: "Blk 27 Lot 25-26",
+	street: 'Onyx Street',
+	subdivision: 'Felizana Estate Subdivision',
+	barangay: 'Pasong Buaya II',
+	city: 'Imus, City',
+	province: 'Cavite',
+	zipCode: 4103
+};
+console.log(fName);
+console.log(lName);
+console.log(age);
+console.log(hobbies);
+console.log(homeAddress);
